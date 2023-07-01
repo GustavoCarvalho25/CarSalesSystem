75062cab0eac104578c2308c3938257a3e92f7dc
Gustavo Carvalho Alves dos Santos <gustavo.carvalho@treetech.com.br>
2023-07-01 17:08:04 -0300

    Funcionamento de tela BuscaCarro finalizada

--- Index.java
+++ Index.java
@@ -323,7 +323,7 @@ public class Index extends JFrame implements ActionListener {
         private JTextField idBuscaTextField;
         private JTextField marcaTextField;
         private JTextField modeloTextField;
-        private JTextField anoTextField;
+        private JTextField precoTextField;
         private JTextField corTextField;
 
         public CarrosPanelBusca() {
@@ -357,7 +357,6 @@ public class Index extends JFrame implements ActionListener {
             JLabel marcaLabel = new JLabel("Marca:");
             marcaLabel.setFont(marcaLabel.getFont().deriveFont(Font.BOLD, 16));
             marcaTextField = new JTextField(20);
-            marcaTextField.setEditable(false);
             constraints.gridx = 0;
             constraints.gridy = 2;
             constraints.insets = new Insets(10, 5, 5, 5);
@@ -368,25 +367,22 @@ public class Index extends JFrame implements ActionListener {
             JLabel modeloLabel = new JLabel("Modelo:");
             modeloLabel.setFont(modeloLabel.getFont().deriveFont(Font.BOLD, 16));
             modeloTextField = new JTextField(20);
-            modeloTextField.setEditable(false);
             constraints.gridy = 4;
             formPanel.add(modeloLabel, constraints);
             constraints.gridy = 5;
             formPanel.add(modeloTextField, constraints);
 
-            JLabel anoLabel = new JLabel("Ano:");
-            anoLabel.setFont(anoLabel.getFont().deriveFont(Font.BOLD, 16));
-            anoTextField = new JTextField(20);
-            anoTextField.setEditable(false);
+            JLabel precoLabel = new JLabel("Preço:");
+            precoLabel.setFont(precoLabel.getFont().deriveFont(Font.BOLD, 16));
+            precoTextField = new JTextField(20);
             constraints.gridy = 6;
-            formPanel.add(anoLabel, constraints);
+            formPanel.add(precoLabel, constraints);
             constraints.gridy = 7;
-            formPanel.add(anoTextField, constraints);
+            formPanel.add(precoTextField, constraints);
 
             JLabel corLabel = new JLabel("Cor:");
             corLabel.setFont(corLabel.getFont().deriveFont(Font.BOLD, 16));
             corTextField = new JTextField(20);
-            corTextField.setEditable(false);
             constraints.gridy = 8;
             formPanel.add(corLabel, constraints);
             constraints.gridy = 9;
@@ -404,62 +400,177 @@ public class Index extends JFrame implements ActionListener {
             constraints.gridy = 11;
             formPanel.add(alterarButton, constraints);
 
-            buscarButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String idBusca = idBuscaTextField.getText().trim();
-
-                    // Verificação e condição
-                    if (idBusca.isEmpty()) {
-                        JOptionPane.showMessageDialog(CarrosPanelBusca.this,
-                                "O campo de busca por ID está vazio.",
-                                "Erro", JOptionPane.ERROR_MESSAGE);
-                    } else if (!idBusca.matches("\\d+")) {
-                        JOptionPane.showMessageDialog(CarrosPanelBusca.this,
-                                "O ID de busca deve conter apenas números.",
-                                "Erro", JOptionPane.ERROR_MESSAGE);
-                    } else {
-                        try {
-                            int carId = Integer.parseInt(idBusca);
-
-                            // Realizar a busca no banco de dados
-                            Class.forName("org.hsql.jdbcDriver");
-                            ResultSet rs = stmt.executeQuery("SELECT * FROM CARRO WHERE CAR_ID = " + carId);
-
-                            if (rs.next()) {
-                                // Preencher os campos de texto com os dados do carro
-                                String marca = rs.getString("MARCA");
-                                String modelo = rs.getString("MODELO");
-                                int ano = rs.getInt("ANO");
-                                String cor = rs.getString("COR");
-
-                                marcaTextField.setText(marca);
-                                modeloTextField.setText(modelo);
-                                anoTextField.setText(String.valueOf(ano));
-                                corTextField.setText(cor);
-                            } else {
-                                JOptionPane.showMessageDialog(CarrosPanelBusca.this,
-                                        "Nenhum carro encontrado com o ID informado.",
-                                        "Aviso", JOptionPane.WARNING_MESSAGE);
-                                marcaTextField.setText("");
-                                modeloTextField.setText("");
-                                anoTextField.setText("");
-                                corTextField.setText("");
-                            }
-
-                            rs.close();
-                            stmt.close();
-                            con.close();
-                        } catch (Exception ex) {
-                            System.out.println("Erro ocorreu em CarrosPanelBusca: " + ex);
-                        }
-                    }
-                }
-            });
-
             mainPanel.add(formPanel, BorderLayout.CENTER);
             add(mainPanel, BorderLayout.CENTER);
             setBorder(BorderFactory.createEmptyBorder(10, 100, 10, 100));
+
+            buscarButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    buscarButtonActionPerformed(e);
+                }
+            });
+
+            excluirButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    excluirButtonActionPerformed(e);
+                }
+            });
+
+            alterarButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    alterarButtonActionPerformed(e);
+                }
+            });
+        }
+
+        private void buscarButtonActionPerformed(ActionEvent e) {
+            String idBusca = idBuscaTextField.getText().trim();
+
+            // Verificação e condição
+            if (idBusca.isEmpty()) {
+                JOptionPane.showMessageDialog(this,
+                        "O campo de busca por ID está vazio.",
+                        "Erro", JOptionPane.ERROR_MESSAGE);
+            } else if (!idBusca.matches("\\d+")) {
+                JOptionPane.showMessageDialog(this,
+                        "O ID de busca deve conter apenas números.",
+                        "Erro", JOptionPane.ERROR_MESSAGE);
+            } else {
+                try {
+                    int carId = Integer.parseInt(idBusca);
+
+                    // Realizar a busca no banco de dados
+                    ResultSet rs = stmt.executeQuery("SELECT * FROM CARRO WHERE CAR_ID = " + carId);
+
+                    if (rs.next()) {
+                        // Preencher os campos de texto com os dados do carro
+                        String marca = rs.getString("MARCA");
+                        String modelo = rs.getString("MODELO");
+                        float preco = rs.getFloat("PRECO");
+                        String cor = rs.getString("COR");
+
+                        marcaTextField.setText(marca);
+                        modeloTextField.setText(modelo);
+                        precoTextField.setText(String.valueOf(preco));
+                        corTextField.setText(cor);
+                    } else {
+                        JOptionPane.showMessageDialog(this,
+                                "Nenhum carro encontrado com o ID informado.",
+                                "Aviso", JOptionPane.WARNING_MESSAGE);
+                        marcaTextField.setText("");
+                        modeloTextField.setText("");
+                        precoTextField.setText("");
+                        corTextField.setText("");
+                    }
+
+                    rs.close();
+                } catch (Exception ex) {
+                    System.out.println("Erro ocorreu em CarrosPanelBusca: " + ex);
+                }
+            }
+        }
+
+        private void excluirButtonActionPerformed(ActionEvent e) {
+            int resposta = JOptionPane.showConfirmDialog(this, "Tem certeza que deseja excluir o carro?", "Confirmação",
+                    JOptionPane.YES_NO_OPTION);
+            if (resposta == JOptionPane.YES_OPTION) {
+                // Obtenha o ID do carro a ser excluído
+                String idBusca = idBuscaTextField.getText().trim();
+                if (idBusca.isEmpty()) {
+                    JOptionPane.showMessageDialog(this, "O campo de busca por ID está vazio.", "Erro",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+                if (!idBusca.matches("\\d+")) {
+                    JOptionPane.showMessageDialog(this, "O ID de busca deve conter apenas números.", "Erro",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+                int carId = Integer.parseInt(idBusca);
+
+                try {
+                    // Realize a exclusão do carro no banco de dados
+                    Class.forName("org.hsql.jdbcDriver");
+
+                    // Execute o comando SQL para excluir o carro
+                    String sql = "DELETE FROM CARRO WHERE CAR_ID = " + carId;
+                    int rowsAffected = stmt.executeUpdate(sql);
+
+                    if (rowsAffected > 0) {
+                        // Limpe os campos de texto
+                        marcaTextField.setText("");
+                        modeloTextField.setText("");
+                        precoTextField.setText("");
+                        corTextField.setText("");
+
+                        JOptionPane.showMessageDialog(this, "Carro excluído com sucesso!", "Sucesso",
+                                JOptionPane.INFORMATION_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(this, "Nenhum carro encontrado com o ID informado.", "Aviso",
+                                JOptionPane.WARNING_MESSAGE);
+                    }
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(this, "Erro ao excluir o carro: " + ex.getMessage(), "Erro",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        }
+
+        private void alterarButtonActionPerformed(ActionEvent e) {
+            // Obtenha o ID do carro a ser alterado
+            String idBusca = idBuscaTextField.getText().trim();
+            if (idBusca.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "O campo de busca por ID está vazio.", "Erro",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            if (!idBusca.matches("\\d+")) {
+                JOptionPane.showMessageDialog(this, "O ID de busca deve conter apenas números.", "Erro",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            int carId = Integer.parseInt(idBusca);
+
+            // Obtenha os novos dados do carro dos campos de texto
+            String marca = marcaTextField.getText().trim();
+            String modelo = modeloTextField.getText().trim();
+            String precoStr = precoTextField.getText().trim();
+            String cor = corTextField.getText().trim();
+
+            if (marca.isEmpty() || modelo.isEmpty() || precoStr.isEmpty() || cor.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "Todos os campos devem ser preenchidos.", "Erro",
+                        JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            try {
+                float preco = Float.parseFloat(precoStr);
+
+                // Realize a alteração do carro no banco de dados
+                Class.forName("org.hsql.jdbcDriver");
+
+                // Execute o comando SQL para alterar o carro
+                String sql = "UPDATE CARRO SET MARCA = '" + marca + "', MODELO = '" + modelo + "', PRECO = " + preco
+                        + ", COR = '" + cor + "' WHERE CAR_ID = " + carId;
+                int rowsAffected = stmt.executeUpdate(sql);
+
+                if (rowsAffected > 0) {
+                    JOptionPane.showMessageDialog(this, "Carro alterado com sucesso!", "Sucesso",
+                            JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(this, "Nenhum carro encontrado com o ID informado.", "Aviso",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(this, "O preço deve ser um valor numérico.", "Erro",
+                        JOptionPane.ERROR_MESSAGE);
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(this, "Erro ao alterar o carro: " + ex.getMessage(), "Erro",
+                        JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 
